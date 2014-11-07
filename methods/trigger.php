294c4e242c0a32f1efedc40e4a40dfec49e44554
jamesdbruner <jamesdbruner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-07 02:35:57 +0000

    fix conflict
    git-svn-id: https://plugins.svn.wordpress.org/wp-modalplate/tags/1.0.0@1021273 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- methods/trigger.php
+++ methods/trigger.php
@@ -9,5 +9,17 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 	});
 	</script>
 
+HEREDOC;=======
+<?php
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+    $trigger .= <<<HEREDOC
+	<script>
+	jQuery(document).ready(function()
+	{	
+ 	   jQuery('.modal-trigger').modalplate();
+	});
+	</script>
+
 HEREDOC;
 ?>
